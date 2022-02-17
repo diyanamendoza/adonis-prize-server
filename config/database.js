e5baa4cade027899a93de3692db5283d78da6e80
Diyana Mendoza <>
2022-02-16 16:25:39 -0800

    Edits

--- config/database.js
+++ config/database.js
@@ -29,15 +29,15 @@ module.exports = {
   | npm i --save sqlite3
   |
   */
-  sqlite: {
-    client: "sqlite3",
-    connection: {
-      filename: Helpers.databasePath(
-        `${Env.get("DB_DATABASE", "development")}.sqlite`
-      ),
-    },
-    useNullAsDefault: true,
-  },
+  // sqlite: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: Helpers.databasePath(
+  //       `${Env.get("DB_DATABASE", "development")}.sqlite`
+  //     ),
+  //   },
+  //   useNullAsDefault: true,
+  // },
 
   /*
   |--------------------------------------------------------------------------
@@ -49,16 +49,16 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: "mysql",
-    connection: {
-      host: Env.get("DB_HOST", "localhost"),
-      port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", "root"),
-      password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "adonis"),
-    },
-  },
+  // mysql: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: Env.get("DB_HOST", "localhost"),
+  //     port: Env.get("DB_PORT", ""),
+  //     user: Env.get("DB_USER", "root"),
+  //     password: Env.get("DB_PASSWORD", ""),
+  //     database: Env.get("DB_DATABASE", "adonis"),
+  //   },
+  // },
 
   /*
   |--------------------------------------------------------------------------
